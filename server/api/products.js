d4e5ef2ae8232413d6a186ae1d9892e6ec2312cf
Devon JohnBaptiste <devondevon@outlook.com>
2017-11-01 13:34:59 -0400

    finished products get 'all' route

--- server/api/products.js
+++ server/api/products.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const router = express().Router;
+const router = require('express').Router();
 const { Product } = require('../db/models');
 
 // router.route let's us chain multiple VERB requests off of the same path
 // Helps keep code DRY
-router.route('/') 
-  .get()
+router.route('/')
+  .get((req, res, next) => {
+    Product.findAll()
+      .then((allProducts) => {
+        res.json(allProducts);
+      })
+      .catch(next);
+  });
+
+module.exports = router;
 
