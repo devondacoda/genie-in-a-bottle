bd807451cc4d12cb79706eb8e3dd9a013e0e4dd4
mcabrex <mrcabardo@gmail.com>
2017-11-03 14:04:49 -0400

    Updated SingleProduct: Added buttons n stuff

--- client/components/SingleProduct.jsx
+++ client/components/SingleProduct.jsx
@@ -8,9 +8,20 @@ function getSingleProduct(props) {
   return (
     <div>
       <h1>{singleProduct.length && singleProduct[0].name}</h1>
-      <img src={singleProduct.length && singleProduct[0].picture} alt="mario messed up"/>
-        <h3>Stock: {singleProduct.length && singleProduct[0].inventory}</h3>
+      <img src={singleProduct.length && singleProduct[0].picture} alt="mario messed up" />
+      <p>{singleProduct.length && singleProduct[0].description}</p>
+      <h3>Stock: {singleProduct.length && singleProduct[0].inventory}</h3>
       <h3>category</h3>
+      <h3>Price: ${singleProduct.length && singleProduct[0].price}.69</h3>
+      <form>
+        <select>
+          <option>Quantity</option>
+          {[...Array(singleProduct.length && singleProduct[0].inventory)].map((option, i) => (
+            <option key={i}>{ i + 1 }</option>
+          ))}
+        </select>
+      </form>
+      <button>Add To Cart</button>
     </div>);
 }
 
