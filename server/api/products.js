4dd643ad99365617e07a05de95e7e0673f119c10
Jonathan Chang <jonathanmchang@gmail.com>
2017-11-04 16:23:58 -0400

    started add product to cart route

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Product } = require('../db/models');
+const { Order } = require('../db/models');
 
 // router.route let's us chain multiple VERB requests off of the same path
 // Helps keep code DRY
@@ -55,6 +56,13 @@ router.route('/:productId')
       .then(() => res.send(`${destroyedProduct} is destroyed`))
       .catch(next);
   });
+
+  // Adding product to cart
+
+  router.put('/:productId/add', (req, res, next) => {
+    const productId = req.params.productId;
+
+  })
 
 module.exports = router;
 
