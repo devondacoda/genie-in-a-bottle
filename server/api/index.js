e30fed8a03baae37c9424d9cdb022b44f0177bc4
Bderiel <bderiel@gmail.com>
2017-11-07 16:40:56 -0500

    feat(eagerloaded products to include reviews)

--- server/api/index.js
+++ server/api/index.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 const productsRouter = require('./products');
 const usersRouter = require('./users');
 const orderRouter = require('./orders');
+// const reviewRouter = require('./reviews');
 
 module.exports = router;
 
 router.use('/users', usersRouter);
 router.use('/products', productsRouter);
 router.use('/orders', orderRouter);
+// router.use('/review', reviewRouter);
 
 router.use((req, res, next) => {
   const error = new Error('Not Found');
